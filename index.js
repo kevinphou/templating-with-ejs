5f3356c045c77000ba48eaa3785f0c17029ec26e
Replit user <>
2023-03-22 03:03:20 +0000

    added updataes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+//kevinphou
 app.set('view engine', 'ejs');
 
 //this will allow us to serve up static files, CSS, images & JS
